a247b51fecc8ef9fc5963c94ba98e4833f84a664
ekallfa <58952444+ekallfa@users.noreply.github.com>
2024-06-23 16:45:37 -0400

    Update deploy_notebooks_2.sql

--- scripts/deploy_notebooks_2.sql
+++ scripts/deploy_notebooks_2.sql
@@ -12,16 +12,9 @@ Last Updated: 6/11/2024
 -- Create the Notebooks
 --USE SCHEMA {{env}}_SCHEMA;
 
-CREATE OR REPLACE NOTEBOOK IDENTIFIER('"DEMO_DB"."{{env}}_SCHEMA"."{{env}}_06_load_excel_files"')
-    FROM '@"DEMO_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/06_load_excel_files/'
+CREATE OR REPLACE NOTEBOOK IDENTIFIER('"DEMO_DB"."{{env}}_SCHEMA"."{{env}}my_file"')
+    FROM '@"DEMO_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/my_file/'
     QUERY_WAREHOUSE = 'DEMO_WH'
-    MAIN_FILE = '06_load_excel_files.ipynb';
+    MAIN_FILE = 'my_file.ipynb';
 
-ALTER NOTEBOOK "DEMO_DB"."{{env}}_SCHEMA"."{{env}}_06_load_excel_files" ADD LIVE VERSION FROM LAST;
-
-CREATE OR REPLACE NOTEBOOK IDENTIFIER('"DEMO_DB"."{{env}}_SCHEMA"."{{env}}_07_load_daily_city_metrics"')
-    FROM '@"DEMO_DB"."INTEGRATIONS"."DEMO_GIT_REPO"/branches/"{{branch}}"/notebooks/07_load_daily_city_metrics/'
-    QUERY_WAREHOUSE = 'DEMO_WH'
-    MAIN_FILE = '07_load_daily_city_metrics.ipynb';
-
-ALTER NOTEBOOK "DEMO_DB"."{{env}}_SCHEMA"."{{env}}_07_load_daily_city_metrics" ADD LIVE VERSION FROM LAST;
+ALTER NOTEBOOK "DEMO_DB"."{{env}}_SCHEMA"."{{env}}my_file" ADD LIVE VERSION FROM LAST;
